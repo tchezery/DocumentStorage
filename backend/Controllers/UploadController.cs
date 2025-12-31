848e1d57f3d18737be79aea491f031ef203f39e4
tchezery <tchezeryribeiro@gmail.com>
2025-12-31 12:11:07 -0300

    Feature: - Code(path) FIles - Download Files By Code (GetFiles)

--- backend/Controllers/UploadController.cs
+++ backend/Controllers/UploadController.cs
@@ -1,13 +1,25 @@
-using System.Reflection.PortableExecutable;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.IO.Compression;
+
+using backend.Data;
+using backend.Models;
+using System.Reflection.PortableExecutable;
 
 [ApiController]
 [Route("api/file")]
-
 public class UploadController : ControllerBase
 {
+    private readonly AppDbContext _context;
+
+    public UploadController(AppDbContext context)
+    {
+        _context = context;
+    }
+
     [HttpPost("upload")]
+    [DisableRequestSizeLimit]
+    [RequestFormLimits(MultipartBodyLengthLimit = long.MaxValue)]
     public async Task<IActionResult> Upload(IFormFile file)
     {
         if (file == null || file.Length == 0)
@@ -15,23 +27,86 @@ public class UploadController : ControllerBase
             return BadRequest("Arquivo não enviado!");
         }
 
-        var folderPath = Path.Combine(
+        var mainFolder = Path.Combine(
             Directory.GetCurrentDirectory(),
             "Uploads"
         );
 
-        if (!Directory.Exists(folderPath))
+        if (!Directory.Exists(mainFolder))
         {
-            Directory.CreateDirectory(folderPath);
+            Directory.CreateDirectory(mainFolder);
         }
 
-        var fullPath = Path.Combine(folderPath, file.FileName);
+        var code = Guid.NewGuid().ToString("N")[..8].ToUpper();
+
+        var fileFolder = Path.Combine(mainFolder, code);
+        Directory.CreateDirectory(fileFolder);
+
+        var originalName = Path.GetFileName(file.FileName);
+        var fullPath = Path.Combine(fileFolder, originalName);
 
         using (var stream = new FileStream(fullPath, FileMode.Create))
         {
             await file.CopyToAsync(stream);
         }
 
-        return Ok("Arquivo armazenado com sucesso!");
+        var fileEntity = new backend.Models.Files
+        {
+            Code = code,
+            FileName = file.FileName,
+            FilePath = fullPath,
+            Size = file.Length
+        };
+
+        _context.Files.Add(fileEntity);
+
+        await _context.SaveChangesAsync();
+
+        return Ok(new
+        {
+            code = code
+        });
+    }
+
+    [HttpGet("download/{code}")]
+    public async Task<IActionResult> Download(string code)
+    {
+        var folderPath = Path.Combine(
+            Directory.GetCurrentDirectory(),
+            "Uploads",
+            code
+        );
+
+        if (!Directory.Exists(folderPath))
+        {
+            return NotFound("Pasta do arquivo não encontrada.");
+        }
+
+        // cria zip em memória
+        using var memoryStream = new MemoryStream();
+
+        using (var zip = new ZipArchive(memoryStream, ZipArchiveMode.Create, true))
+        {
+            var files = Directory.GetFiles(folderPath);
+
+            foreach (var filePath in files)
+            {
+                var fileName = Path.GetFileName(filePath);
+                var entry = zip.CreateEntry(fileName);
+
+                using var entryStream = entry.Open();
+                using var fileStream = System.IO.File.OpenRead(filePath);
+
+                await fileStream.CopyToAsync(entryStream);
+            }
+        }
+
+        memoryStream.Position = 0;
+
+        return File(
+            memoryStream.ToArray(),
+            "application/zip",
+            $"{code}.zip"
+        );
     }
 }
--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41a56237b96c6d36ee468b3a5a1dd3043180657e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd55e8e4bda9dd98fee36daa398b896f12c4fac3")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
